d9d1d253aefc3a18ea9d39aaad2c34763687c9dd
Ilia Kim <i.kim@campus.tu-berlin.de>
2024-09-17 15:08:40 +0200

    name: Thuy

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ app.post("/api/chat", async (req, res) => {
       waitingForConfirmation: true,
       customerNumber: null,
       //IMPORTANT, change later!!
-      userName: "Nikolas",
+      userName: "Thuy",
       group: group,
       taskFlags: {
         trackOrderACompleted: false,
